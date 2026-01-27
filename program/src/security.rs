c54493573a5e97db8066b76774356f3f74c0b724
jeffyanta <jeff@flipcash.com>
2026-01-27 09:48:24 -0500

    Update Security.txt

--- program/src/security.rs
+++ program/src/security.rs
@@ -3,7 +3,7 @@ use solana_security_txt::security_txt;
 
 #[cfg(not(feature = "no-entrypoint"))]
 security_txt! {
-    name: "Flipcash Currency Creator Program",
+    name: "Flipcash Reserve Contract",
     project_url: "https://flipcash.com",
     contacts: "email:security@flipcash.com",
     policy: "https://github.com/code-payments/flipcash-program/blob/main/SECURITY.md",
